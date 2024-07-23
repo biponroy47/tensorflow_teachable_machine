8bc166737ec5194ec7d1dbdfe37d5d41d11c85b8
Bipon Roy <biponroy47@yahoo.com>
2024-07-23 11:27:15 -0400

    load model

--- script.js
+++ script.js
@@ -1,19 +1,67 @@
-/**
- * @license
- * Copyright 2018 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
+const STATUS = document.getElementById('status')
+const VIDEO = document.getElementById('webcam')
+const ENABLE_CAM_BUTTON = document.getElementById('enableCam')
+const RESET_BUTTON = document.getElementById('reset')
+const TRAIN_BUTTON = document.getElementById('train')
+const MOBILE_NET_INPUT_WIDTH = 224
+const MOBILE_NET_INPUT_HEIGHT = 224
+const STOP_DATA_GATHER = -1
+const CLASS_NAMES = []
 
-const status = document.getElementById('status')
-status.innerText = 'Loaded TensorFlow.js - version: ' + tf.version.tfjs
+ENABLE_CAM_BUTTON.addEventListener('click', enableCam)
+TRAIN_BUTTON.addEventListener('click', trainAndPredict)
+RESET_BUTTON.addEventListener('click', reset)
+
+function enableCam() {
+  // TODO: Fill this out later in the codelab!
+}
+
+function trainAndPredict() {
+  // TODO: Fill this out later in the codelab!
+}
+
+function reset() {
+  // TODO: Fill this out later in the codelab!
+}
+
+let dataCollectorButtons = document.querySelectorAll('button.dataCollector')
+for (let i = 0; i < dataCollectorButtons.length; i++) {
+  dataCollectorButtons[i].addEventListener('mousedown', gatherDataForClass)
+  dataCollectorButtons[i].addEventListener('mouseup', gatherDataForClass)
+  // Populate the human readable names for classes.
+  CLASS_NAMES.push(dataCollectorButtons[i].getAttribute('data-name'))
+}
+
+function gatherDataForClass() {
+  // TODO: Fill this out later in the codelab!
+}
+
+let mobilenet = undefined
+let gatherDataState = STOP_DATA_GATHER
+let videoPlaying = false
+let trainingDataInputs = []
+let trainingDataOutputs = []
+let examplesCount = []
+let predict = false
+
+/**
+ * Loads the MobileNet model and warms it up so ready for use.
+ **/
+async function loadMobileNetFeatureModel() {
+  const URL =
+    'https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v3_small_100_224/feature_vector/5/default/1'
+
+  mobilenet = await tf.loadGraphModel(URL, { fromTFHub: true })
+  STATUS.innerText = 'MobileNet v3 loaded successfully!'
+
+  // Warm up the model by passing zeros through it once.
+  tf.tidy(function () {
+    let answer = mobilenet.predict(
+      tf.zeros([1, MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH, 3])
+    )
+    console.log(answer.shape)
+  })
+}
+
+// Call the function immediately to start loading.
+loadMobileNetFeatureModel()
